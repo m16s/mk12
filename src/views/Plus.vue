852adb6715c4833e0b39f0ba47e532971dfd5f9a
Maksims Strasinskis <m.strasinskis@gmail.com>
2018-03-29 13:04:53 +0300

    Add new Plus functionality

--- src/views/Plus.vue
+++ src/views/Plus.vue
@@ -1,12 +1,29 @@
 <template>
   <div>
-    Plus
+    What good have you done?
+    <input v-model.trim="description">
+
+    <button>BACK</button>
+    <button @click="addPlus()">ADD PLUS</button>
   </div>
 </template>
 
 <script>
-export default {
+import {ADD_PLUS_ACTION} from '@/store'
 
+export default {
+  data: () => ({
+    description: '',
+  }),
+
+  methods: {
+    addPlus() {
+      this.$store.dispatch({
+        type: ADD_PLUS_ACTION,
+        description: this.description,
+      })
+    }
+  },
 }
 </script>
 
